8e288ad235b1cd9f4e07e291daa0e845c994c0c4
SandhyaBadgujar <32235605+SandhyaBadgujar@users.noreply.github.com>
2023-03-14 09:44:46 +0000

    Delete Demo11_Custommodule directory

--- Demo11_Custommodule/modules/storageaccount/output.tf
+++
@@ -1,9 +0,0 @@
-output "storageid" {
-  description = "This is the id of the provisioned storage accoutn"
-  value = azurerm_storage_account.sandhya-storage.id
-}
-
-output "StorageTier" {
-  description = "The tier of the storage account"
-  value = azurerm_storage_account.sandhya-storage.access_tier
-}
